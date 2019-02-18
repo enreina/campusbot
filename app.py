ab576b5ac6b1570c621f362b2b02573e75187228
Enreina Annisa Rizkiasri <enreina@gmail.com>
2019-02-18 17:48:50 +0100

    set webhook and add requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from telegram.ext import Updater, CommandHandler
 import logging
+from dotenv import load_dotenv, find_dotenv
+import os
 from dialoguemanager.response import generalCopywriting
 
-TELEGRAM_BOT_API_TOKEN = '555780883:AAEcrvyhYbYn_uzRZAdtSc8i3F4dPmTDuog'
+load_dotenv(find_dotenv())
 
-updater = Updater(token=TELEGRAM_BOT_API_TOKEN)
+NGROK_CAMPUSBOT_URL = os.getenv('NGROK_CAMPUSBOT_URL')
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+PORT = int(os.getenv('PORT'))
+
+updater = Updater(token=TELEGRAM_BOT_TOKEN)
 dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -17,5 +23,6 @@ def start(bot, update):
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-updater.start_polling()
+updater.start_webhook(listen='0.0.0.0', port=PORT, url_path=TELEGRAM_BOT_TOKEN, webhook_url=NGROK_CAMPUSBOT_URL+'/'+TELEGRAM_BOT_TOKEN)
+updater.bot.set_webhook(NGROK_CAMPUSBOT_URL+'/'+TELEGRAM_BOT_TOKEN)
 updater.idle()

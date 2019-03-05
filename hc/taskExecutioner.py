af6452de44a42b5acc08f72aaebfe6f88606b343
Enreina Annisa Rizkiasri <enreina@gmail.com>
2019-03-05 17:28:57 +0100

    use inline keyboard for validation task

--- hc/taskExecutioner.py
+++ hc/taskExecutioner.py
@@ -1,6 +1,7 @@
 from db.task import Task
 from db.item import Item
-from telegram.ext import CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackQueryHandler
 from dialoguemanager.response import generalCopywriting
 import db.firestoreClient as FirestoreClient
 from utility.placeUtility import findNearestPlaceItem
@@ -29,6 +30,8 @@ class TaskExecutioner(object):
             self.currentQuestionNumber = 0
 
             bot.send_message(chat_id=update.message.chat_id, text=self.task.openingStatement, parse_mode='Markdown')
+            if self.task.type == self.TASK_TYPE_VALIDATE_ITEM and 'image' in self.questionData:
+                bot.send_photo(chat_id=update.message.chat_id, photo=self.questionData['image'], parse_mode='Markdown')
             self.sendCurrentQuestion(bot, update)
 
             return self.currentQuestionNumber
@@ -70,8 +73,10 @@ class TaskExecutioner(object):
                 handler = MessageHandler(Filters.photo, questionHandler)
             elif question['type'] == 'location':
                 handler = MessageHandler(Filters.location, questionHandler)
-            elif question['type'] in ['text', 'multiple-input', 'location-name', 'image-single-validation']:
+            elif question['type'] in ['text', 'multiple-input', 'location-name']:
                 handler = MessageHandler(Filters.text, questionHandler)
+            elif question['type'].startswith('single-validation'):
+                handler = CallbackQueryHandler(questionHandler)
             states[questionNumber] = [handler]
             
             if 'confirmationText' in question: 
@@ -96,9 +101,14 @@ class TaskExecutioner(object):
 
     def initQuestionData(self):
         # load items with corresponding itemType
-        self.questionData = Item.getItemsByType(self.task.itemType)[1] # TO-DO assign item here
+        self.questionData = Item.getItemsByType(self.task.itemType)[0] # TO-DO assign item here
 
     def sendCurrentQuestion(self, bot, update):
+        if update.callback_query:
+            chatId = update.callback_query.message.chat_id
+        else:
+            chatId = update.message.chat_id
+
         currentQuestion = self.task.questions[self.currentQuestionNumber]
         if self.task.type == self.TASK_TYPE_CREATE_ITEM:
             formattedQuestion = currentQuestion['text'].format(item=self.temporaryAnswer)
@@ -117,23 +127,27 @@ class TaskExecutioner(object):
             for place in nearbyPlaces:
                 keyboardReply.append([place['name']])
             replyMarkup = {"keyboard": keyboardReply}
-        elif currentQuestion['type'] == 'image-single-validation':
-            replyMarkup = {"keyboard": generalCopywriting.VALIDATE_ANSWER_KEYBOARD}
-
+        elif currentQuestion['type'].startswith('single-validation'):
+            keyboard = [[InlineKeyboardButton(generalCopywriting.VALIDATE_ANSWER_YES_TEXT, callback_data='0'),
+                        InlineKeyboardButton(generalCopywriting.VALIDATE_ANSWER_NO_TEXT, callback_data='1')],
+                        [InlineKeyboardButton(generalCopywriting.VALIDATE_ANSWER_NOT_SURE_TEXT, callback_data='2')]]
+            replyMarkup = InlineKeyboardMarkup(keyboard)
         else:
             replyMarkup = {"remove_keyboard": True}
-
         
-        if currentQuestion['type'] == 'image-single-validation':
-            bot.send_photo(chat_id=update.message.chat_id, photo=self.questionData['image'], caption=formattedQuestion, reply_markup=replyMarkup, parse_mode='Markdown')
-        else:
-            bot.send_message(chat_id=update.message.chat_id, text=formattedQuestion, reply_markup=replyMarkup, parse_mode='Markdown')
+        if currentQuestion['type'] == 'single-validation-location':
+            bot.send_location(chat_id=chatId, latitude=self.questionData['location']['latitude'], longitude=self.questionData['location']['longitude'])
+        bot.send_message(chat_id=chatId, text=formattedQuestion, reply_markup=replyMarkup, parse_mode='Markdown')
 
     def sendCurrentQuestionResponse(self, bot, update):
+        if update.callback_query:
+            chatId = update.callback_query.message.chat_id
+        else:
+            chatId = update.message.chat_id
         currentQuestion = self.task.questions[self.currentQuestionNumber]
         formattedResponseOk = currentQuestion['responseOk'].format(item=self.temporaryAnswer)
         replyMarkup = {"remove_keyboard": True}
-        bot.send_message(chat_id=update.message.chat_id, text=formattedResponseOk, reply_markup=replyMarkup, parse_mode='Markdown')
+        bot.send_message(chat_id=chatId, text=formattedResponseOk, reply_markup=replyMarkup, parse_mode='Markdown')
 
     def saveTemporaryAnswer(self, bot, update):
         currentQuestion = self.task.questions[self.currentQuestionNumber]
@@ -148,37 +162,50 @@ class TaskExecutioner(object):
             splittedAnswers = [x.strip() for x in update.message.text.split(',')]
             self.temporaryAnswer[propertyName] = update.message.text
             self.temporaryAnswer[propertyName + '-list'] = splittedAnswers
+        elif currentQuestion['type'].startswith('single-validation'):
+            self.temporaryAnswer[propertyName] = update.callback_query.data
+
+        print(self.temporaryAnswer)
         
     def sendConfirmation(self, bot, update):
+        if update.callback_query:
+            chatId = update.callback_query.message.chat_id
+        else:
+            chatId = update.message.chat_id
+
         for question in self.task.questions:
             if 'confirmationText' in question:
                 formattedConfirmation = question['confirmationText'].format(item=self.temporaryAnswer)
                 if question['type'] in ['text', 'multiple-input', 'location-name']:
-                    bot.send_message(chat_id=update.message.chat_id, text=formattedConfirmation, parse_mode='Markdown')
+                    bot.send_message(chat_id=chatId, text=formattedConfirmation, parse_mode='Markdown')
                 elif question['type'] == 'image':
                     image = self.temporaryAnswer[question['property']]
-                    bot.send_photo(chat_id=update.message.chat_id, photo=image, caption=formattedConfirmation, parse_mode='Markdown')
+                    bot.send_photo(chat_id=chatId, photo=image, caption=formattedConfirmation, parse_mode='Markdown')
                 elif question['type'] == 'location':
                     location = self.temporaryAnswer[question['property']]
-                    bot.send_message(chat_id=update.message.chat_id, text=formattedConfirmation, parse_mode='Markdown')
-                    bot.send_location(chat_id=update.message.chat_id, latitude=location['latitude'], longitude=location['longitude'])
+                    bot.send_message(chat_id=chatId, text=formattedConfirmation, parse_mode='Markdown')
+                    bot.send_location(chat_id=chatId, latitude=location['latitude'], longitude=location['longitude'])
         # send is that correct
         replyMarkup = {"keyboard": [[generalCopywriting.YES_BUTTON_TEXT], [generalCopywriting.NO_BUTTON_TEXT]]}
-        bot.send_message(chat_id=update.message.chat_id, text=generalCopywriting.ASK_DATA_CONFIRMATION_TEXT, reply_markup=replyMarkup, parse_mode='Markdown')
+        bot.send_message(chat_id=chatId, text=generalCopywriting.ASK_DATA_CONFIRMATION_TEXT, reply_markup=replyMarkup, parse_mode='Markdown')
 
 
     def sendClosingStatement(self, bot, update):
+        if update.callback_query:
+            chatId = update.callback_query.message.chat_id
+        else:
+            chatId = update.message.chat_id
+
         if self.task.type == self.TASK_TYPE_CREATE_ITEM:
             formattedClosingStatement = self.task.closingStatement.format(item=self.temporaryAnswer)
         else:
             formattedClosingStatement = self.task.closingStatement.format(item=self.questionData)
 
         replyMarkup = {"remove_keyboard": True}
-        bot.send_message(chat_id=update.message.chat_id, text=formattedClosingStatement, reply_markup=replyMarkup, parse_mode='Markdown')
+        bot.send_message(chat_id=chatId, text=formattedClosingStatement, reply_markup=replyMarkup, parse_mode='Markdown')
 
     def saveAnswers(self):
         if self.task.type == self.TASK_TYPE_CREATE_ITEM: # task type 
             data = self.temporaryAnswer
             data['itemType'] = self.task.itemType
             FirestoreClient.saveDocument('items', data=data)
-

7c4371b84784d9eb748b1c098a15aa23eccf8621
Enreina Annisa Rizkiasri <enreina@gmail.com>
2019-03-07 17:03:12 +0100

    use bunch when loading task

--- db/task.py
+++ db/task.py
@@ -1,18 +1,10 @@
 import firestoreClient as FirestoreClient
+from bunch import Bunch
 
 class Task(object):
-
-    def __init__(self, openingStatement, closingStatement, taskType, entryCommand=None, questions=[], itemType=None):
-        self.openingStatement = openingStatement
-        self.closingStatement = closingStatement
-        self.type = taskType
-        self.entryCommand = entryCommand
-        self.questions = questions
-        self.itemType = itemType
-
     @staticmethod
     def getTaskById(taskId):
         taskDictionary = FirestoreClient.getDocument('tasks', taskId, populate=False)
-        return Task(taskDictionary['openingStatement'], taskDictionary['closingStatement'], taskDictionary['type'], taskDictionary['entryCommand'], taskDictionary['questions'], taskDictionary['itemType'])
+        return Bunch(taskDictionary)
 
     

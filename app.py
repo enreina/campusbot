007ffa7173fbde9135850ea32de8f3e9ad3ce161
Enreina Annisa Rizkiasri <enreina@gmail.com>
2019-04-01 10:37:23 +0200

    add task execuiter for creating trash bin

--- app.py
+++ app.py
@@ -32,6 +32,9 @@ dispatcher.add_handler(enrichPlaceExecutioner.conversationHandler)
 createMealExecutioner = TaskExecutioner('create-meal')
 dispatcher.add_handler(createMealExecutioner.conversationHandler)
 
+createTrashBinExecutioner = TaskExecutioner('create-trash-bin')
+dispatcher.add_handler(createTrashBinExecutioner.conversationHandler)
+
 updater.start_webhook(listen='0.0.0.0', port=env.PORT, url_path=env.TELEGRAM_BOT_TOKEN, webhook_url=env.NGROK_CAMPUSBOT_URL+'/'+env.TELEGRAM_BOT_TOKEN)
 updater.bot.set_webhook(env.NGROK_CAMPUSBOT_URL+'/'+env.TELEGRAM_BOT_TOKEN)
 updater.idle()

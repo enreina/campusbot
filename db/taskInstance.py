653f62310f56e950f6226b8538e54db03bbe6e35
Enreina Annisa Rizkiasri <enreina@gmail.com>
2019-04-06 13:38:36 +0200

    load task instances and build a list of messages

---
+++ db/taskInstance.py
@@ -0,0 +1,29 @@
+from google.cloud.firestore_v1beta1.document import DocumentReference 
+import firestoreClient as FirestoreClient
+from bunch import Bunch
+from pprint import pprint
+from common.constants.taskType import TASK_TYPE_AS_STRING
+
+class TaskInstance(object):
+    @staticmethod
+    def get_task_instances_for_user(user, task_instance_collection_name='placeTaskInstances'):
+        if type(user) == dict:
+            userId = user['_id']
+        else:
+            userId = user 
+        pprint(userId)
+        # get task instances
+        task_instances = FirestoreClient.getDocuments(task_instance_collection_name, [
+            ('userId', '==', userId)])
+        pprint(task_instances)
+        # populate task inside each task instance
+        for task_instance in task_instances:
+            task_instance['task'] = task_instance['task'].get().to_dict()
+            task_instance['task']['item'] = task_instance['task']['item'].get().to_dict()
+
+            task_instance['title'] = task_instance['task']['item']['name'] # for place
+            task_instance.taskTypeAsString = TASK_TYPE_AS_STRING[task_instance['task']['type']]
+             
+        return [Bunch(task_instance) for task_instance in task_instances]
+
+    
--- tasklist/taskListHandler.py
+++ tasklist/taskListHandler.py
@@ -1,5 +1,7 @@
 from telegram.ext import Updater, CommandHandler
 from db.user import User
+from db.taskInstance import TaskInstance
+from pprint import pprint
 
 class TaskListHandler:
 
@@ -11,13 +13,32 @@ class TaskListHandler:
         self.entry_command_handler = CommandHandler(entry_command, self._entry_command_callback)
         self.dispatcher.add_handler(self.entry_command_handler)
 
+    def build_task_list_message(self, user):
+        taskInstances = TaskInstance.get_task_instances_for_user(user, task_instance_collection_name=self.task_instance_collection_name)
+        messages = []
+        for idx,taskInstance in enumerate(taskInstances):
+            # build message
+            taskInstanceTitle = taskInstance.title
+            task = taskInstance.task
+            item = task['item']
+            command = "/{canonical_name}{idx}".format(canonical_name=self.canonical_name, idx=idx+1)
+            preview_url = "http://campusbot.cf/task-preview?title={taskTypeAsString}&imageurl={item[image]}&itemtype={canonical_name}".format(
+                taskTypeAsString=taskInstance.taskTypeAsString,
+                item=item,
+                canonical_name=self.canonical_name
+            )
+            message = u"{command} <b>{title}</b><a href='{preview_url}'>\u200f</a>".format(command=command, title=taskInstanceTitle, preview_url=preview_url)
+            messages.append(message)
+        return messages
 
     def _entry_command_callback(self, update, context):
         bot = context.bot
         chatId = update.message.chat_id
         userTelegramId = unicode(update.message.from_user.id)
+        context.chat_data['user'] = User.getUserById(userTelegramId)
+        user = context.chat_data['user']
 
         # to do, implement to send list of tasks
-        bot.send_message(chat_id=chatId, text=u"/place1 <b>TU Delft Library</b> <a href='http://campusbot.cf/task-preview?title=Validate&imageurl=https://d1rkab7tlqy5f1.cloudfront.net/_processed_/2/7/csm_Contactinfo%20gebouw%20buitenkant_54b704d5fa.jpg&itemtype=Place'>\u200f</a>",parse_mode='HTML')
+        for message in self.build_task_list_message(user):
+            bot.send_message(chat_id=chatId, text=message,parse_mode='HTML')
         
-        context.chat_data['user'] = User.getUserById(userTelegramId)

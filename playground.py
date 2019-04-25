5c0c5b33d5bb65be62494ecedede9e19c4f64c0f
Enreina Annisa Rizkiasri <enreina@gmail.com>
2019-04-25 13:28:39 +0200

    Update playground.py

--- playground.py
+++ playground.py
@@ -3,7 +3,7 @@ import datetime
 from google.cloud import firestore
 from db.taskTemplate import TaskTemplate
 import json
-from pprint import pprint
+from pprint import 
 from google.cloud.firestore_v1beta1.document import DocumentReference
 from db.firestoreClient import db
 
@@ -12,8 +12,8 @@ from db.firestoreClient import db
 # db.collection('users').document('156992599_bak').set(user.to_dict())
 
 # copy task instances to new user
-# for x in ['questionTaskInstances', 'foodTaskInstances', 'placeTaskInstances', 'trashBinTaskInstances']:
-#     taskInstances = db.collection('users').document('156992599').collection(x).get()
-#     taskInstancesRef = db.collection('users').document('156992599_bak').collection(x)
-#     for instance in taskInstances:
-#         taskInstancesRef.add(instance.to_dict())
+for x in ['questionTaskInstances', 'foodTaskInstances', 'placeTaskInstances', 'trashBinTaskInstances']:
+     taskInstances = db.collection('users').document('156992599').collection(x).get()
+     taskInstancesRef = db.collection('users').document('641517503').collection(x)
+     for instance in taskInstances:
+         taskInstancesRef.add(instance.to_dict())

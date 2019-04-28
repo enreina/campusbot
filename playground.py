3900893e49b3c280741ac8e3dc8a7f4ae57c6ac4
Enreina Annisa Rizkiasri <enreina@gmail.com>
2019-04-28 14:09:20 +0200

    update playground to manually assign task

--- playground.py
+++ playground.py
@@ -12,8 +12,8 @@ from db.firestoreClient import db
 # db.collection('users').document('156992599_bak').set(user.to_dict())
 
 # copy task instances to new user
-# for x in ['questionTaskInstances', 'foodTaskInstances', 'placeTaskInstances', 'trashBinTaskInstances']:
-#     taskInstances = db.collection('users').document('156992599').collection(x).get()
-#     taskInstancesRef = db.collection('users').document('156992599_bak').collection(x)
-#     for instance in taskInstances:
-#         taskInstancesRef.add(instance.to_dict())
+for x in ['questionTaskInstances', 'foodTaskInstances', 'placeTaskInstances', 'trashBinTaskInstances']:
+    taskInstances = db.collection('users').document('156992599_bak').collection(x).get()
+    taskInstancesRef = db.collection('users').document('156992599').collection(x)
+    for instance in taskInstances:
+        taskInstancesRef.add(instance.to_dict())

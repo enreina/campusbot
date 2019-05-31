e5f3f338a9bdc6a82e2117b120b4322e82366763
Enreina Annisa Rizkiasri <enreina@gmail.com>
2019-05-31 21:15:53 +0200

    playground user count

--- playground.py
+++ playground.py
@@ -18,12 +18,37 @@ from db.firestoreClient import db
 #      for instance in taskInstances:
 #          taskInstancesRef.add(instance.to_dict())
 
-# task template create-place
-createPlaceTemplate = db.collection('taskTemplates').document('create-place').get().to_dict()
-questionsCreatePlace = createPlaceTemplate['questions']
-taskTemplates = db.collection('taskTemplates').get()
+excludeUsers = ['i.p.samiotis@tudelft.nl', 's.qiu-1@tudelft.nl', 'nehasreet@gmail.com', 'nehasreenishi@gmail.com', 'enreina@gmail.com', '641517503', '20791653', '178336114', '271561032', '874886396', '555780883', '444883337', '827717938', '156992599']
 
-for task in taskTemplates:
-    questions = task.to_dict()['questions']
-    questions.insert(len(questions), questionsCreatePlace[-1])
-    db.collection('taskTemplates').document(task.id).update({'questions': questions})
+getUsers = db.collection('users').get()
+allUsers = [x for x in getUsers]
+chatbotv1Users = []
+chatbotv2Users = []
+mobileAppUsers = []
+
+activeUsers = 0
+for user in allUsers:
+    userData = user.to_dict()
+    numTasksCompleted = len(userData.get('tasksCompleted', []))
+    if userData.get('telegramId', False) and userData['telegramId'] not in excludeUsers:
+        if userData.get('chatbotv2', False):
+            chatbotv2Users.append(userData)
+            if (numTasksCompleted > 2):
+                activeUsers = activeUsers + 1
+        else:
+            chatbotv1Users.append(userData)
+    elif userData.get('email', False) and userData['email'] not in excludeUsers:
+        mobileAppUsers.append(userData)
+        # if (numTasksCompleted > 1):
+        #     activeUsers = activeUsers + 1
+
+    
+
+print([x['telegramId'] for x in chatbotv1Users])
+print(len(chatbotv1Users))
+print([x['telegramId'] for x in chatbotv2Users])
+print(len(chatbotv2Users))
+print(len(mobileAppUsers))
+print(activeUsers)
+
+
